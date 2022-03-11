6d5ff38ae8444a0cea95aa0767e94f8748b1aac2
Geovanny Silva <geovanny@eutsiv.com>
2022-03-11 16:21:09 -0300

    backend: album, author and music endpoints

--- http/backend/app.js
+++ http/backend/app.js
@@ -6,6 +6,9 @@ const app = new koa()
 
 koaRouter
   .get('/', async ctx => ctx.body = 'Hello World!')
+  .get('/api/v1/album', async ctx => ctx.body = 'Album!')
+  .get('/api/v1/author', async ctx => ctx.body = 'Author!')
+  .get('/api/v1/music', async ctx => ctx.body = 'Music!')
 
 app
   .use(koaRouter.routes())
