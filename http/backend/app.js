e4d74223313b866990a171bc327ded23e715f42f
Geovanny Silva <geovanny@eutsiv.com>
2022-03-18 14:57:37 -0300

    pg module added and db connection created

--- http/backend/app.js
+++ http/backend/app.js
@@ -1,10 +1,20 @@
 const koa = require('koa')
 const koaRouter = require('koa-router')()
 
+const { Pool } = require('pg')
+
 const albumEndpoint = require('./endpoint/api/v1/album')
 const authorEndpoint = require('./endpoint/api/v1/author')
 const musicEndpoint = require('./endpoint/api/v1/music')
 
+
+const pool = new Pool({
+  user: 'root',
+  host: 'localhost',
+  database: 'app',
+  password: 'root',
+  port: 5432
+})
 
 const app = new koa()
 
