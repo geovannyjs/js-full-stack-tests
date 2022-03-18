078d7b579b354329f2a4502fdb410c2643b565e5
Geovanny Silva <geovanny@eutsiv.com>
2022-03-18 15:23:06 -0300

    added koa-body module, endpoint for inserting author

--- http/backend/endpoint/api/v1/author.js
+++ http/backend/endpoint/api/v1/author.js
@@ -1,6 +1,18 @@
+const koaBody = require('koa-body')
+
+
 module.exports = ({ router, db }) => {
 
-  router.get('/api/v1/author', async ctx => ctx.body = 'Author!')
+  router
+    .get('/api/v1/author', async ctx => ctx.body = 'Author!')
+    .post('/api/v1/author', koaBody(), async ctx => {
+
+      let json = ctx.request.body
+
+      return db.query('insert into author (name, is_active) values ($1, $2)', [json.name, json.isActive])
+        .then(() => ctx.body = { success: true })
+
+    })
 
   return router
 
