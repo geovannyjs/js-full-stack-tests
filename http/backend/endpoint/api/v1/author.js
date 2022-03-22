546706f72f86e77942c3f7321d5b414b15e76284
geordanjunio <geordanjunio@gmail.com>
2022-03-22 12:14:32 -0300

    Alteração SQL Tempalte String

--- http/backend/endpoint/api/v1/author.js
+++ http/backend/endpoint/api/v1/author.js
@@ -1,15 +1,18 @@
 const koaBody = require('koa-body')
+const SQL = require ('sql-template-strings') 
+
 
 
 module.exports = ({ router, db }) => {
-
+ 
   router
-    .get('/api/v1/author', async ctx => db.query('select * from author').then(r => ctx.body = { rows: r.rows }))
+    .get('/api/v1/author', async ctx => db.query(SQL`select * from author`)
+      .then(r => ctx.body = { rows: r.rows }))
     .post('/api/v1/author', koaBody(), async ctx => {
 
       let json = ctx.request.body
 
-      return db.query('insert into author (name, is_active) values ($1, $2)', [json.name, json.isActive])
+      return db.query(SQL`insert into author (name, is_active) values (${json.name}, ${json.isActive})`)
         .then(() => ctx.body = { success: true })
 
     })
